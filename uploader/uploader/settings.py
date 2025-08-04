e944e2e8694db4f4cd31591c947c9423096f7884
Vladyslav Gusakovskiy <vladyslav.gusakovskiy@gmail.com>
2025-08-04 21:19:21 +0300

    fixed the issue in the settings.py - SECRET_KEY not found

--- uploader/uploader/settings.py
+++ uploader/uploader/settings.py
@@ -23,6 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = config('SECRET_KEY', default='test-secret-key')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = config('DEBUG', default=True, cast=bool)
+
 ALLOWED_HOSTS = ['*']
 
 
@@ -135,6 +141,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=True, cast=bool)
