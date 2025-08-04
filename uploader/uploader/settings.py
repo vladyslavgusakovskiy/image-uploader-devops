32ea4c036cb7ec4ccd2e89fc1ab2be178c237fbb
Vladyslav Gusakovskiy <vladyslav.gusakovskiy@gmail.com>
2025-08-04 20:47:09 +0300

    fixed issue with multiple imports on one line

--- uploader/uploader/settings.py
+++ uploader/uploader/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
-import os, sys
+import os
+import sys
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
